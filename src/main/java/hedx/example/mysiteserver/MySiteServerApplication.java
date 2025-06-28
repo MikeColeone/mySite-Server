b75e3fb875a3512677d6b09a12cbb8ac024a2b6a
MikeColeone <coleonemike@gmail.com>
2025-06-28 23:01:20 +0800

    int: sql初始化

---
+++ src/main/java/hedx/example/mysiteserver/MySiteServerApplication.java
@@ -0,0 +1,13 @@
+package hedx.example.mysiteserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySiteServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySiteServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hedx/example/mysiteserver/MySiteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package hedx.example.mysiteserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySiteServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
